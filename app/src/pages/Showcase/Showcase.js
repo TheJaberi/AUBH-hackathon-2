1c3cd8cf8bebb2a08002d99feb41e1850d040c82
ajaberi <adnan.ajaberi@gmail.com>
2025-02-13 11:46:03 +0300

    update to paths for 3d models

--- app/src/pages/Showcase/Showcase.js
+++ app/src/pages/Showcase/Showcase.js
@@ -4,10 +4,10 @@ import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
 // GLB file paths and labels
 const modelPaths = [
-  '/models/tripo_pbr_model_9e5ba12b-5f45-4ea3-93a3-89a18cc11e3a.glb',
-  '/models/tripo_pbr_model_93406153-93cc-4fd0-89e7-6eeea79c51ea.glb',
-  '/models/tripo_pbr_model_a1bb976b-f0d2-43f8-9db1-a3d57ec04bc8.glb',
-  '/models/tripo_pbr_model_cbcf7196-6f2e-4405-bb82-5934fbe8fa14.glb'
+  '/src/assets/privacy.glb',
+  '/src/assets/bias.glb',
+  '/src/assets/manipulation.glb',
+  '/src/assets/earth.glb'
 ];
 
 const modelLabels = [
